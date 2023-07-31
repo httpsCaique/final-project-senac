e85b68ba4a13fafb390e303d0d3a97e6fd27d93f
YGuilhermo13 <guilherminhoh003@gmail.com>
2023-07-31 11:40:12 -0300

    login

--- login.php
+++ login.php
@@ -41,18 +41,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/estiloadm.css">
+    <link rel="stylesheet" href="./css/estilo.css">
     <title>LOGIN USUARIO</title>
 </head>
 <body>
-    <form action="login.php" method="post">
+
+
+<video autoplay loop muted >
+    <source src="./video/backvideo.mp4" type="video/mp4">
+  </video >
+
+
+    <form action="login.php" method="post" class="log">
         <h1>LOGIN DE USUARIO</h1>
         <input type="text" name="nome" placeholder="NOME" required>
         <p></p>
         <input type="password" name="senha" placeholder="SENHA" required>
         <p></p>
-        <input type="submit" name="login" value="LOGIN">
+        <button  class="button-54" class="" role="button" type="submit" name="login" value="LOGIN">LOGIN</button>
     </form>
     
+<div class="log"  >
+    <h1 >Não tem Cadastro?</h1>
+    <p>Faça seu Cadastro Aqui!</p>
+    <button  class="button-54" role="button">CADASTRE</button>
+</div>
+
 </body>
 </html>
