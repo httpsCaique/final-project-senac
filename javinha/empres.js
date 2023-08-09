e076ae20d802868f1c935fc0e130172ef63bcfff
YGuilhermo13 <guilherminhoh003@gmail.com>
2023-08-09 11:40:25 -0300

    melhoria tela empresa

--- javinha/empres.js
+++ javinha/empres.js
@@ -1,24 +1,31 @@
 function calcempresa() {
-  var lucroBruto = Number(document.getElementById("emp1").value);
-  var gastoFuncionarios = Number(document.getElementById("emp2").value);
-  var materiaPrima = Number(document.getElementById("emp3").value);
-  var gastosOperacionais = Number(document.getElementById("emp4").value);
-  var gastoImposto = Number(document.getElementById("emp5").value);
+    var val1 = Number(document.getElementById("emp1").value);
+    var val2 = Number(document.getElementById("emp2").value);
+    var val3 = Number(document.getElementById("emp3").value);
+    var val4 = Number(document.getElementById("emp4").value);
+    var val5 = Number(document.getElementById("emp5").value);
+    var val6 = Number(document.getElementById("emp6").value);
+    var val7 = Number(document.getElementById("emp7").value);
+    var val8 = Number(document.getElementById("emp8").value);
+  
 
-  var gastoTotal = gastoFuncionarios + materiaPrima + gastosOperacionais + gastoImposto;
-  var subtracaoTotal = lucroBruto - gastoTotal;
+    var gasto =  val2 + val3 + val4 + val5 + val6 + val7 + val8 ;
+    var gastoTotal =  gasto * -1 ;
+    var subtracaoTotal = val1 - gasto;
+  
+    document.getElementById("gasto").textContent = gastoTotal.toFixed(2);
+    document.getElementById("lucro").textContent = subtracaoTotal.toFixed(2);
 
-  document.getElementById("gasto").textContent = gastoTotal;
-  document.getElementById("lucro").textContent = subtracaoTotal;
 
-  var ctx = document.getElementById('myChart').getContext('2d');
+
+    var ctx = document.getElementById('myChart').getContext('2d');
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
       labels: ['Gasto Total', 'Lucro Líquido'],
       datasets: [{
         label: 'Valores',
-        data: [gastoTotal, subtracaoTotal],
+        data: [gasto, subtracaoTotal],
         backgroundColor: ['rgba(255, 99, 132, 0.6)', 'rgba(54, 162, 235, 0.6)'],
         borderColor: ['rgba(0,0,0)', 'rgba(0,0,0)'],
         borderWidth: 5 
@@ -33,16 +40,8 @@ function calcempresa() {
     }
   });
 
-}
 
-
-
-
-
-
-
-
-  
+  }
   
   
   
@@ -52,12 +51,12 @@ function calcempresa() {
     document.getElementById("emp3").value = "";
     document.getElementById("emp4").value = "";
     document.getElementById("emp5").value = "";
+    document.getElementById("emp6").value = "";
+    document.getElementById("emp7").value = "";
+    document.getElementById("emp8").value = "";
     document.getElementById("gasto").textContent = "";
     document.getElementById("lucro").textContent = "";
   }
-
-
-
   
  
 
