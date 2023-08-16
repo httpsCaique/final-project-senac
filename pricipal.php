3f2f435b5729b5f001e2daddae5eb4ce92bf58b2
YGuilhermo13 <guilherminhoh003@gmail.com>
2023-08-16 11:48:42 -0300

    k

--- pricipal.php
+++ pricipal.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="javinha/index1.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/ling.css">
     <link rel="stylesheet" href="css/nome.css">
     <link rel="stylesheet" href="css/carrousel.css">
     <title>Principal</title>
